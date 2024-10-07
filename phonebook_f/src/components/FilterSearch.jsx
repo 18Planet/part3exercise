e8c5035466d0bcd4f0494ae16b192ae6b9a1b31a
Gavin Cartier <48884797+18Planet@users.noreply.github.com>
2024-10-07 16:20:17 -0700

    dkksd

--- phonebook_f/src/components/FilterSearch.jsx
+++
@@ -1,5 +0,0 @@
-const FilterSearch = ({filter, updateFilter}) => {
-    return (<div>Filter search: <input value = {filter} onChange = {updateFilter}></input></div>)
-}
-
-export default FilterSearch
